a5d249f6173e9116f20cd2fe785fc51bea65c99e
Evan Herman <evan.m.herman@gmail.com>
2022-02-01 19:36:34 -0500

    Run on page load

--- remove-paywall.js
+++ remove-paywall.js
@@ -1,36 +1,82 @@
-// Gete the remove-paywall button
-let removePaywallBtn = document.getElementById( 'remove-paywall' );
-
-// When the button is clicked, inject removePaywall into current page
-removePaywallBtn.addEventListener( 'click', async () => {
-  let [tab] = await chrome.tabs.query( { active: true, currentWindow: true } );
-
-  chrome.scripting.executeScript( {
-    target: { tabId: tab.id },
-    function: removePaywall,
-  } );
-} );
-
 // The body of this function will be execueted as a content script inside the current page
 function removePaywall() {
-  let screenOverlay = document.querySelector( '.tp-backdrop' );
-  screenOverlay.parentNode.removeChild( screenOverlay );
+  // Add a 200ms timeout to wait for the DOM content to appear
+  setTimeout(function() {
+    removeAds();
 
-  let screenOverlayContainer = document.querySelector( '.tp-modal' );
-  screenOverlayContainer.parentNode.removeChild( screenOverlayContainer );
+    // Check if paywall exists befoore altering the DOM
+    let element = document.querySelector( '.tp-modal' );
+    if ( null === element ) {
+      return;
+    }
 
-  let stickyAd = document.querySelector( '#sticky-anchor' );
-  stickyAd.parentNode.removeChild( stickyAd );
+    // Screen Overlay
+    hideElement( '.tp-backdrop' );
 
-  let mainAd = document.querySelector( '#x-reveal-ad' );
-  if ( null !== mainAd ) {
-    mainAd.parentNode.removeChild( mainAd );
+    // Paywall Modal
+    hideElement( '.tp-modal' );
+
+    // Sticky Footer Ad
+    hideElement( '#sticky-anchor' );
+
+    // Remove the body class preventing scrolling
+    const body = document.querySelector( 'body' );
+    body.classList.remove( 'tp-modal-open' );
+
+    console.log( "LancasterOnline paywall removed." );
+  }, 200 );
+}
+
+function removeAds() {
+  // Main Ad
+  hideElement( '#x-reveal-ad' );
+
+  // revcontent
+  hideElement( '.lnp-revcontent-wrapper' );
+
+  // Hide unwanted elements on the page
+  // ie: share buttons, sidebar ads, in-content ads etc.
+  hideAllElements( '.share-container, .tnt-ads-container' );
+
+  console.log( "LancasterOnline ads removed." );
+}
+
+/**
+ * Hide a single element on the page
+ *
+ * @param  {string} elementSelector The element to remove.
+ *
+ * @return {null}
+ */
+function hideElement( elementSelector ) {
+  let element = document.querySelector( elementSelector );
+  if ( null !== element ) {
+    element.parentNode.removeChild( element );
   }
+}
 
-  const body = document.querySelector( 'body' );
-  body.classList.remove( 'tp-modal-open' );
-
-  document.querySelectorAll( '.share-container, .sticky-anchor, .tncms-block, .tncms-region' ).forEach( function( blockToRemove ) {
-    blockToRemove.remove();
+/**
+ * Hide multiple elements on the page.
+ *
+ * @param  {string} elementSelectors The elements to remove, separted by a comma.
+ *
+ * @return {null}
+ */
+function hideAllElements( elementSelectors ) {
+  document.querySelectorAll( elementSelectors ).forEach( function( blockToRemove ) {
+    if ( null !== blockToRemove ) {
+      blockToRemove.remove();
+    }
   } );
+}
+
+// Hide the ads and paywall after everything else loads.
+if ( 'complete' == document.readyState ) {
+  removePaywall();
+} else {
+  document.onreadystatechange = function () {
+    if ( 'complete' == document.readyState ) {
+      removePaywall();
+    }
+  }
 }
